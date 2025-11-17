4c7ca44267582774ff0ff14c0f8b6e7bbb92dd14
ian <ian.thangng@gmail.com>
2025-11-17 07:58:23 +0700

    update order expiration + reserved or not

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,11 +3,10 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@ianticketing/common';
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
-
+import { deleteOrderRouter } from "./routes/delete";
+import { showOrderRouter } from "./routes/show";
+import { indexOfRouter } from "./routes";
+import { newOrderRouter } from "./routes/new";
 
 const app = express();
 app.set('trust proxy', true);
@@ -20,10 +19,10 @@ app.use(
 );
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(deleteOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOfRouter);
+app.use(newOrderRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
