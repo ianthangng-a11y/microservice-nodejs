a4bd2c4544c019d99757fa0059b58848657060dd
ian <ian.thangng@gmail.com>
2025-11-21 20:11:07 +0700

    add versioning for updated/created

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -52,7 +52,7 @@ router.post('/api/orders',
     await order.save();
     
     // Publish an event saying that an order was created.
-    new OrderCreatedPublisher(natsWrapper.client).publish({
+    await new OrderCreatedPublisher(natsWrapper.client).publish({
       id: order.id,
       status: order.status,
       userId: order.userId,
@@ -60,7 +60,8 @@ router.post('/api/orders',
       ticket: {
         id: ticket.id,
         price: ticket.price
-      }
+      },
+      version: order.version,
     });
 
     res.status(201).send(order);
