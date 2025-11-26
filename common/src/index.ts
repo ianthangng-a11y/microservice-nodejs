8aa9270639a974354287258564efabcef4615283
ian <ian.thangng@gmail.com>
2025-11-26 17:25:51 +0700

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -11,13 +11,13 @@ export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
 
-export * from './event/subject';
-export * from './event/base-publisher';
-export * from './event/base-listener';
-export * from './event/ticket-created-event';
-export * from './event/ticket-updated-event';
+export * from './events/subject';
+export * from './events/base-publisher';
+export * from './events/base-listener';
+export * from './events/ticket-created-event';
+export * from './events/ticket-updated-event';
 
-export * from './event/types/order-status';
-export * from './event/order-cancelled-event';
-export * from './event/order-created-event';
-export * from './event/expiration-complete-event';
+export * from './events/types/order-status';
+export * from './events/order-cancelled-event';
+export * from './events/order-created-event';
+export * from './events/expiration-complete-event';
