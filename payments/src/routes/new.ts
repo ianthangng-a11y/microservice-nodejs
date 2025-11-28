4d2bd763acc077c80264b33bc757be7f36b909df
ian <ian.thangng@gmail.com>
2025-11-28 09:54:19 +0700

    install + setup stripe api

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
 } from "@ianticketing/common";
 import { body } from "express-validator";
 import { Order } from "../models/order";
+import { stripe } from "../stripe";
 
 const router = express.Router();
 
@@ -35,6 +36,11 @@ router.post('/api/payments', requireAuth,
       throw new BadRequestError('Cannot pay for a cancelled order.');
     }
     
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token,
+    })
     res.send({ success: true });
 });
 
